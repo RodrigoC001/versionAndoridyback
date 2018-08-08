116f270da93395d71f4a5818e1b31fcc6c8eb045
Alan Sainz <alanmsainz@gmail.com>
2018-08-08 11:32:37 -0300

    adds working mapbox boiler

--- working_mapbox/aire_test/AireTest/App.js
+++ working_mapbox/aire_test/AireTest/App.js
@@ -15,23 +15,30 @@ import Mapbox from '@mapbox/react-native-mapbox-gl';
 
 Mapbox.setAccessToken('pk.eyJ1IjoibGF1dGFyb2dyYW5kZSIsImEiOiJjamtrNjFqMW8xbnVhM3BwYjdmZjczcXkyIn0._Gz0SnZDQIGeosDSbwFwMA');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component<{}> {
+  renderAnnotations () {
+    return (
+      <Mapbox.PointAnnotation
+        key='pointAnnotation'
+        id='pointAnnotation'
+        coordinate={[11.254, 43.772]}>
 
-type Props = {};
-export default class App extends Component<Props> {
+        <View style={styles.annotationContainer}>
+          <View style={styles.annotationFill} />
+        </View>
+        <Mapbox.Callout title='Look! An annotation!' />
+      </Mapbox.PointAnnotation>
+    )
+  }
   render() {
     return (
       <View style={styles.container}>
         <Mapbox.MapView
-          styleURL={Mapbox.StyleURL.Street}
+          styleURL={Mapbox.StyleURL.Dark}
           zoomLevel={15}
           centerCoordinate={[11.256, 43.770]}
           style={styles.container}>
+          {this.renderAnnotations()}
         </Mapbox.MapView>
       </View>
     );
@@ -42,14 +49,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderRadius: 15,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'orange',
+    transform: [{ scale: 0.6 }],
+  }
 });
