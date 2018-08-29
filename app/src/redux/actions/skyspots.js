7a86a4fe2fd5f660a1e773a63f645d115e487157
Alan Sainz <alanmsainz@gmail.com>
2018-08-29 11:51:56 -0300

    adds basic form for creating skyspots

--- app/src/redux/actions/skyspots.js
+++ app/src/redux/actions/skyspots.js
@@ -36,3 +36,43 @@ export function getSkyspotsRequest() {
       });
   };
 }
+
+export function postSkyspotAttempt() {
+  return {
+    type: "POST_SKYSPOT_ATTEMPT"
+  };
+}
+
+export function postSkyspotSuccess(response) {
+  return {
+    type: "POST_SKYSPOT_SUCCESS",
+    payload: {
+      response: response.data
+    }
+  };
+}
+
+export function postSkyspotFailure(error) {
+  return {
+    type: "POST_SKYSPOT_FAILURE",
+    payload: {
+      error
+    }
+  };
+}
+
+export function postSkyspot(body, success, failure) {
+  return dispatch => {
+    dispatch(postSkyspotAttempt());
+    return axios
+      .post('/api/skyspot/', body)
+      .then(response => {
+        success()
+        return dispatch(postSkyspotSuccess(response));
+      })
+      .catch(error => {
+        failure()
+        return dispatch(postSkyspotFailure(error))
+      });
+  };
+}
