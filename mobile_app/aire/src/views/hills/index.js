0b4d1da2af6e680ee8fe4aff5fc32a2efef9727f
Alan Sainz <alanmsainz@gmail.com>
2018-08-30 12:47:05 -0300

    removes header links on mobile version of dashboard, updates mobile version with hills view

--- mobile_app/aire/src/views/hills/index.js
+++ mobile_app/aire/src/views/hills/index.js
@@ -4,14 +4,27 @@ import {Platform, StyleSheet, Text, View, Image, ImageBackground, ScrollView} fr
 export default (props) => (
   <View style={s.container}>
     <ImageBackground source={require('../../assets/fondotrivia/fondotrivia.png')} style={{width: '100%', height: '100%'}}>
+  <ScrollView>
     <View style={s.titleContainer}>
       <Text style={s.titleText}>
         ¿SOS UN EXPLORADOR DEL AIRE?
       </Text>
     </View>
     <View style={s.windowContainer}>
-      <Image source={require('../../assets/ventana_avion/ventana_avion.png')} />
+      <Image source={require('../../assets/ventana_avion/ventana_avion.png')} style={{width: 263, height: 338}} />
     </View>
+    <View style={s.buttonsContainer}>
+
+    <View style={s.buttonPosition}>
+      <View style={s.buttonContainer}>
+        <Text style={s.textButton}>
+          Bañado la Estrella
+        </Text>
+      </View>
+    </View>
+
+    </View>
+    </ScrollView>
     </ImageBackground>
   </View>
 )
@@ -24,6 +37,28 @@ const s = StyleSheet.create({
     // flex: 1,
     paddingHorizontal: 70,
     marginTop: 45
+  },
+  buttonsContainer: {
+    marginTop: 20,
+  },
+  buttonPosition: {
+    // backgroundColor: 'orange',
+    paddingHorizontal: 69,
+    height: 40,
+    justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  buttonContainer: {
+    borderRadius: 16,
+    height: 32,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textButton: {
+    fontSize: 12,
+    color: 'rgb(64,76,155)',
+    fontFamily: 'HouschkaRoundedAltMedium'
   },
   windowContainer: {
     backgroundColor: 'transparent',
