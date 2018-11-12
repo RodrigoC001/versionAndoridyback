6963f89178516c9a83b3eddf687eb5ce13b0f123
Eric <alanmsainz@gmail.com>
2018-11-12 18:38:14 -0300

    adds icons for android

--- mobile_app/aireapp/src/config/server.js
+++ mobile_app/aireapp/src/config/server.js
@@ -1,2 +1,2 @@
-export const ip = "http://aire.dev5.io/api";
-// export const ip = "http://10.15.11.77/api";
+// export const ip = "http://aire.dev5.io/api";
+export const ip = "http://192.168.0.26/api";
