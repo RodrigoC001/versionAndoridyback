0d9e3cb96299140865239c966969f159e94f25e1
Eric <alanmsainz@gmail.com>
2018-11-13 12:49:21 -0300

    connects the mobile app with the dev5 temporal server

--- mobile_app/aireapp/src/config/server.js
+++ mobile_app/aireapp/src/config/server.js
@@ -1,2 +1,2 @@
-// export const ip = "http://aire.dev5.io/api";
-export const ip = "http://192.168.0.26/api";
+export const ip = "http://aire.dev5.io/api";
+// export const ip = "http://192.168.0.26/api";
